e8a57d42a760cd3f821cf90f27c11a0783a77a09
Arnold Waigwa <agw13@albion.edu>
2026-03-31 13:20:00 -0400

    add form fields and simplemde

--- app/request-a-ride/page.tsx
+++ app/request-a-ride/page.tsx
@@ -1,13 +1,41 @@
+"use client";
 import { Flex, Box, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import React, { useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 //location, description, price
 
 const page = () => {
+  const [data, setData] = useState({ location: "", description: "" });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <Flex>
+    <Flex direction="column">
       <Box width="500px" mt="4">
-        <TextField.Root placeholder="location..." size="3" />
+        <TextField.Root
+          placeholder="location..."
+          size="3"
+          value={data.location}
+          name="location"
+          onChange={handleChange}
+        />
+      </Box>
+      <Box width="500px" mt="4">
+        <TextField.Root
+          placeholder="description..."
+          size="3"
+          value={data.description}
+          name="description"
+          onChange={handleChange}
+        />
+      </Box>
+      <Box mt="4">
+        <SimpleMDE placeholder="Testing Simplemde..." />
       </Box>
     </Flex>
   );
