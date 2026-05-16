64f4df5a7cbd4c0ae400feb88d1018d55bdd34a5
Arnold Waigwa <agw13@albion.edu>
2026-05-15 21:08:32 -0400

    Configure credentials provider alternative

--- app/auth.ts
+++ app/auth.ts
@@ -1,7 +1,20 @@
 import prisma from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
+import { LoginSchema } from "./ValidationSchema";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+import { DefaultSession } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -10,10 +23,54 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {label: "Email", type: "email", placeholder: "Jsmith@albion.edu"},
+        password: {label: "Password", type: "password", placeholder: "1234.."}
+      },
+      async authorize(credentials){
+        const validation = LoginSchema.safeParse(credentials);
+        if (!validation.success) return null;
+
+        const {email, password} = validation.data;
+
+        const user = await prisma.user.findUnique({where: {email}});
+        
+        if (!user || !user.password) return null; 
+        //if user registered with google: no password set
+        //compare passwords
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+
+        if (!passwordMatch) return null;
+
+        return {
+          id: String(user.id),
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        }
+      }
+    })
   ],
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    async jwt({token, user}) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({session, token}) {
+      if (session.user) {
+        session.user.id = token.id as string;   
+      }
+      return session
+    }
+  }
 };
 
 export default authOptions;
