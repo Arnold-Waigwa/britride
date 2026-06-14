26e1a824d8be4b13ec5b4820ef6ed6e166df148e
Arnold Waigwa <agw13@albion.edu>
2026-06-14 01:00:26 -0400

    Implement middleware to enforce session

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for static assets and auth pages
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|login|register).*)",
+  ],
+};
