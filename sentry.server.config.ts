0eb816c099f28eaef4bbb692530269089b12a1c8
Arnold Waigwa <agw13@albion.edu>
2026-07-02 18:15:19 -0400

    Implement sentry tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c72de23b792e83711256c2abc6b0bf3e@o4511663196667904.ingest.us.sentry.io/4511663204794368",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
